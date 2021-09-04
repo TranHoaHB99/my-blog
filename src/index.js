7e9af47516e80fd44ba91a9fa82c7949cab821d0
TranHoa1 <tranhoahb99@gmail.com>
2021-09-04 10:14:18 +0700

    add to github

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-
+import 'normalize.css'
+import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
