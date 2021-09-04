d6b99180d92177daf5fbc20fc4a3a0421226cb4d
TranHoa1 <tranhoahb99@gmail.com>
2021-09-04 10:52:43 +0700

    add something

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
-
+import React,{Suspense} from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom'; 
+import Spinner from './components/Spinner/Spinner'
+import Header from './components/Header/Header'
+import NotFound from './components/NotFound/NotFound'
+import ErrorBoundary from './ErrorBoundary'
 function App() {
   return (
     <div>
-    
+     <Suspense fallback={<Spinner/>}>
+        <BrowserRouter>
+          <ErrorBoundary>
+            <Header />
+            <Switch>
+              <Redirect exact from='/' to='/' />
+              {/* <Route path='/posts' component={Post} />
+              <Route path='/login' component={Login} /> */}
+              <Route component={NotFound} />
+            </Switch>
+          </ErrorBoundary>
+        </BrowserRouter>
+      </Suspense>
+      
     </div>
   );
 }
